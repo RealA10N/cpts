9445476bffddededcc460022256c2b09c60e3917
Alon Krymgand <me@alon.kr>
2023-09-04 14:16:17 +0300

    Added rich exception printing

--- cpts/errors/base.py
+++ cpts/errors/base.py
@@ -1,6 +1,42 @@
+from rich.align import Align
+from rich.console import Group
+from rich.padding import Padding
+from rich.panel import Panel
+from rich.text import Text
+
+
 class CPTSBaseError(Exception):
     """A base (excepted) error, that when raised is caught by the CLI."""
 
-    def __init__(self, title: str, description: str | None = None) -> None:
+    def __init__(
+        self, title: str, description: str, exit_code: int = 1
+    ) -> None:
         self.title = title
         self.description = description
+        self.exit_code = exit_code
+
+    def __str__(self) -> str:
+        return str(
+            Text(
+                "[bold white on red] :boom: Error [/] [bold]"
+                + f"{self.title}:[/bold] {self.description}"
+                + f"[italic bright_black] exited with code {self.exit_code}[/]"
+            )
+        )
+
+    def __rich__(self) -> str:
+        return Group(
+            Panel(
+                f"[bold]{self.title}:[/bold] {self.description}",
+                border_style="red",
+                title=":boom: Error!",
+            ),
+            Align(
+                Padding(
+                    f"exited with code {self.exit_code}",
+                    pad=(0, 2, 0, 0),
+                    style="italic bright_black",
+                ),
+                align="right",
+            ),
+        )
